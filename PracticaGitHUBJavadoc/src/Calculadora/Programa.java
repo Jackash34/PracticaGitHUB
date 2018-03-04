86f2226dd3981b5b442743d242dbbaed161accb5
miguel <miguel@MintEntornos>
2018-03-04 02:16:16 +0100

    Calculadora finalizada y operativa
    En este commit se han finalizado los métodos necesarios para que la
    calculadora (clase Calculadora) funcione correctamente según las pautas
    establecidas.
    También se ha realizado la clase Programa, la cual ejecuta todas las
    funciones de la clase Calculadora y se prueba que tengas resultados
    existosos en todas las posibilidades que brindan los métodos.

--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -2,10 +2,31 @@ package Calculadora;
 
 public class Programa {
 	
-	//TODO: Crear un programa principal que 
-	//utilice todas las funciones de la calculadora
-	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
-	//Ejecutadlo.
+	public static void main(String[] args) {
+		//TODO: Crear un programa principal que 
+		//utilice todas las funciones de la calculadora
+		//Incluida las operaciones toString y la potencia
+		//que habrï¿½is aï¿½adido.
+		//Ejecutadlo.
+		
+		Calculadora calculadora = new Calculadora(5, 7);
+		
+		System.out.println(calculadora.suma());
+		System.out.println(calculadora.resta());
+		System.out.println(calculadora.division());
+		System.out.println(calculadora.potencia());
+		System.out.println(calculadora);
+		
+		Calculadora calculadora2 = new Calculadora(5, 0);
+		System.out.println(calculadora2.potencia());
+		
+		Calculadora calculadora3 = new Calculadora(2, -3);
+		System.out.println(calculadora3.potencia());
+		
+		/*Calculadora finalizada y operativa
 
+En este commit se han finalizado los mÃ©todos necesarios para que la calculadora (clase Calculadora) funcione correctamente segÃºn las pautas establecidas.
+TambiÃ©n se ha realizado la clase Programa, la cual ejecuta todas las funciones del */
+		
+	}
 }
